32a13561f70d7fdaa6695846a6d0e07df28daf0a
RubenErhardt <rubengamebattles@outlook.com>
2025-04-11 10:00:29 +0200

    github api en react

--- mijn-react-test/src/App.jsx
+++ mijn-react-test/src/App.jsx
@@ -3,15 +3,26 @@ import './App.css'
 import Header from  './componenten/Header'
 import Counter from './componenten/Counter';
 import InlogStatus from './componenten/Inlogstatus';
-import Navbar from './componenten/Navbar';
+import {useEffect} from 'react';
 
 function App() {
   const [text, setText] = useState("");
+  const [githubData, setGithubData] = useState([])
+const [githubUser, setGithubUser] = useState("")
+
+const fetchData = () => {
+  return fetch(`https://api.github.com/users/${githubUser}`)
+    .then((response) => response.json())
+    .then((data) => setGithubData(data));
+}
+
+useEffect(() => {
+  fetchData()
+}, [])
 
   return (
     <>
       <Header />
-      <Navbar />
       <div>
         <h2>Typ iets:</h2>
         <input 
@@ -20,6 +31,10 @@ function App() {
           onChange={(e) => setText(e.target.value)}
         />
         <p>Jij schreef: {text}</p>
+        <input type="text" placeholder="Search for User" onChange={(e) => setGithubUser(e.target.value)} className="input_search" />
+<button onClick={fetchData} className="search_button">Search Github</button>
+<img src={githubData.avatar_url} height="100" width="100" />
+<p>{githubData.name}</p>
       </div>
       <Counter />
       <InlogStatus />
